ee4283be5ad446107b48d1cbf4b75713719ad940
Thelina Geogre <George.thelina@gmail.com>
2019-09-25 20:31:07 -0400

    spotify keys added

--- keys.js
+++ keys.js
@@ -1,7 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.cc8a054a95454c34b16d41498c0d6b37,
+  secret: process.env.0e4d96e0ae154b6c82b0edb2f6cd2c27
+
 };
 
